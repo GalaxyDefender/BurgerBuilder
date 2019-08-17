1a3d6737488d16478ff61ae1cfbe5b594b290675
Quentin <qgerard88@gmail.com>
2019-08-17 22:21:58 +0200

    custom data input

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,37 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Aux from '../Auxilary/Auxilary';
-import classes from './Layout.css';
-import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
-import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import Aux from "../Auxilary/Auxilary";
+import classes from "./Layout.css";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 
 class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  };
 
-    state= {
-        showSideDrawer: false
-    }
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
 
-    sideDrawerClosedHandler = () => {
-        this.setState({showSideDrawer: false});
-    }
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => {
+      return { showSideDrawer: !this.state.showSideDrawer };
+    });
+  };
 
-    sideDrawerToggleHandler = () => {
-        this.setState((prevState) => {
-            return {showSideDrawer: !this.state.showSideDrawer};
-        });
-    }
-
-    render() {
-        return(
-            <Aux>
-                <Toolbar drawToggleClicked={this.sideDrawerToggleHandler}/>
-                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
-                <main className={classes.Content}>
-                    {this.props.children}
-                </main>
-            </Aux>   
-        )
-    }
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
 }
 
 export default Layout;
