ebb552daaa3edb4c8bb3a1fd5832331fe9ccba7e
Quentin <qgerard88@gmail.com>
2019-08-18 17:33:46 +0200

    Fixing an error

--- src/container/Checkout/ContactData/ContactData.js
+++ src/container/Checkout/ContactData/ContactData.js
@@ -85,7 +85,8 @@ class ContactData extends Component {
           ]
         },
         valid: true,
-        value: ""
+        value: "",
+        validation: {}
       }
     },
     formIsValid: false,
@@ -120,6 +121,10 @@ class ContactData extends Component {
 
   checkValidity(value, rules) {
     let isValid = true;
+
+    if (!rules) {
+      return true;
+    }
 
     if (rules.required) {
       isValid = value.trim() !== "" && isValid;
