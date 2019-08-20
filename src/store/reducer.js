a3ba0cf3b513ad67e8de0f7f4b027c62241395b2
Quentin <qgerard88@gmail.com>
2019-08-20 17:38:37 +0200

    fixed  the action type issue and added Total price state

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -10,15 +10,23 @@ const initialState = {
   totalPrice: 4
 };
 
+const INGRENDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+};
+
 const reducer = (state = initialState, action) => {
-  switch (action.types) {
+  switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
       return {
         ...state,
         ingredients: {
           ...state.ingredients,
           [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-        }
+        },
+        totalPrice: state.totalPrice + INGRENDIENT_PRICES[action.ingredientName]
       };
     case actionTypes.REMOVE_INGREDIENT:
       return {
@@ -26,7 +34,8 @@ const reducer = (state = initialState, action) => {
         ingredients: {
           ...state.ingredients,
           [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-        }
+        },
+        totalPrice: state.totalPrice - INGRENDIENT_PRICES[action.ingredientName]
       };
     default:
       return state;
