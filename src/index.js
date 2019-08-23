cf76c53dd9153f6a692cb2b45c3ba4a77866e544
Quentin <qgerard88@gmail.com>
2019-08-23 11:02:20 +0200

    preparing folder structure

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
-import reducer from "./store/reducer";
+import reducer from "./store/reducer/burgerBuilder";
 
 const store = createStore(
   reducer,
